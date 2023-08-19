fa1f8df2c2ff37beac03a6031573f8e04c293eed
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-08-19 12:46:00 +0200

    comment

--- Modulo JS/esercizio5.js
+++ Modulo JS/esercizio5.js
@@ -9,3 +9,6 @@ const person1 = {
   person2.firstName = "Simon";
   console.log(person1);
   console.log(person2);
+
+//per gli oggetti, come per le funzioni, vale la proprietà "pass by reference". 
+//Per questo motivo, il cambiamento di un valore che equivale ad un altro agisce direttamente su quello originale.
